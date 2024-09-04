c9e45396669ea42b112ed27843f3d8e36fd10f0c
Mechwart Tanulo <mechwart.tanulo@burg.lo.hu>
2024-09-04 10:10:15 +0200

    done

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 "use strict";
-let myName = "Slim Shady";
-console.log(myName);
+const numEl = document.getElementById("inputNum");
+const result = document.getElementById("result");
+const theNumber = Math.floor(Math.random() * 100) + 1;
+console.log(theNumber);
+function guess() {
+    const guessNumber = numEl.valueAsNumber;
+    if (isCorrect(guessNumber)) {
+        result.innerText = "Eltaláltad";
+    }
+    else {
+        if (guessNumber < theNumber) {
+            result.innerText = "A gondolt szám nagyobb.";
+        }
+        else {
+            result.innerText = "A gondolt szám kisebb";
+        }
+    }
+}
+function isCorrect(guessNumber) {
+    return guessNumber === theNumber;
+}
--- index.ts
+++ index.ts
@@ -1,2 +1,24 @@
-let myName: string = "Slim Shady"
-console.log(myName);
+const numEl = document.getElementById("inputNum") as HTMLInputElement
+
+const result = document.getElementById("result") as HTMLParagraphElement
+ 
+const theNumber = Math.floor(Math.random() * 100) + 1
+
+console.log(theNumber)
+
+function guess() {
+    const guessNumber: number = numEl.valueAsNumber
+    if (isCorrect(guessNumber)) {
+        result.innerText = "Eltaláltad"
+    } else {
+        if (guessNumber < theNumber) {
+            result.innerText = "A gondolt szám nagyobb."
+        } else {
+            result.innerText = "A gondolt szám kisebb"
+        }
+    }
+}
+
+function isCorrect(guessNumber: number) :boolean {
+    return guessNumber === theNumber;
+}
